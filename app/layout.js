926475b4725f4eaa56a02179e58900b8550cbc74
Davids P. <davidspetuhovs@icloud.com>
2024-12-17 11:02:36 +0200

    took out daisyui, installed shadcn

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,6 @@ const font = Inter({ subsets: ["latin"] });
 
 export const viewport = {
 	// Will use the primary color of your theme to show a nice theme color in the URL bar of supported browsers
-	themeColor: config.colors.main,
 	width: "device-width",
 	initialScale: 1,
 };
@@ -21,7 +20,6 @@ export default function RootLayout({ children }) {
 	return (
 		<html
 			lang="en"
-			data-theme={config.colors.theme}
 			className={font.className}
 		>
 			<body>
--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-import themes from "daisyui/src/theming/themes";
-
 const config = {
   // REQUIRED
   appName: "ShipFast",
@@ -77,13 +75,6 @@ const config = {
     fromAdmin: `David at QAlileo <David@mail.qalileo.com>`,
     // Email shown to customer if need support. Leave empty if not needed => if empty, set up Crisp above, otherwise you won't be able to offer customer support."
     supportEmail: "david@qalileo.com",
-  },
-  colors: {
-    // REQUIRED — The DaisyUI theme to use (added to the main layout.js). Leave blank for default (light & dark mode). If you any other theme than light/dark, you need to add it in config.tailwind.js in daisyui.themes.
-    theme: "light",
-    // REQUIRED — This color will be reflected on the whole app outside of the document (loading bar, Chrome tabs, etc..). By default it takes the primary color from your DaisyUI theme (make sure to update your the theme name after "data-theme=")
-    // OR you can just do this to use a custom color: main: "#f37055". HEX only.
-    main: themes["light"]["primary"],
   },
   auth: {
     // REQUIRED — the path to log in users. It's use to protect private routes (like /dashboard). It's used in apiClient (/libs/api.js) upon 401 errors from our API
