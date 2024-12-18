a5b35ea809d48976070cecefe9bad9be3ae5a510
Davids P. <davidspetuhovs@icloud.com>
2024-12-18 16:34:02 +0200

    test div

--- app/onboarding/page.js
+++ app/onboarding/page.js
@@ -40,6 +40,7 @@ export default function LoginPage() {
       className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
       <div className="w-full max-w-xs">
         <OnboardingForm />
+        <div></div>
       </div>
     </div>)
   );
